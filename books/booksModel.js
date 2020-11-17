05011e3749ca4192b9312d49584c6657e6b6162f
Abdi Mohamud <abdinajibmohamud@gmail.com>
2020-11-16 17:31:08 -0800

    added post,put, and delete requests

--- books/booksModel.js
+++ books/booksModel.js
@@ -2,12 +2,26 @@ const db = require("../data/db-config");
 
 module.exports = {
   find,
-//   findById,
-//   insert,
-//   edit,
-//   remove,
+  findById,
+  insert,
+  edit,
+  remove,
 };
 
 function find() {
-    return db("books");
+  return db("books");
+}
+function findById(id) {
+  return db("books").where({ id }).first();
+}
+async function insert(book) {
+  const [newItem] = await db("books").insert(book, "*");
+  return newItem;
+}
+async function edit(id, changes) {
+  const [changed] = await db("books").where({ id }).update(changes, "*");
+  return changed;
+}
+function remove(id) {
+  return db("books").where({ id }).del();
 }
--- books/booksRouter.js
+++ books/booksRouter.js
@@ -6,4 +6,28 @@ router.get("/", (req, res) => {
     .then((book) => res.status(200).json(book))
     .catch((err) => res.status(500).json({ message: err.message }));
 });
+
+router.get("/:id", (req, res) => {
+  Books.findById(req.params.id)
+    .then((book) => res.status(200).json(book))
+    .catch((err) => res.status(500).json({ message: err.message }));
+});
+
+router.post("/", (req, res) => {
+  Books.insert(req.body)
+    .then((book) => res.status(201).json(book))
+    .catch((err) => res.status(500).json({ message: err.message }));
+});
+router.put("/:id", (req, res) => {
+  const { id } = req.params;
+  Books.edit(id, req.body)
+    .then((book) => res.status(203).json(book))
+    .catch((err) => res.status(500).json({ message: err.message }));
+});
+router.delete("/:id", (req, res) => {
+  Books.remove(req.params.id)
+    .then((book) => res.status(201).json(book))
+    .catch((err) => res.status(500).json({ message: err.message }));
+});
+
 module.exports = router;
